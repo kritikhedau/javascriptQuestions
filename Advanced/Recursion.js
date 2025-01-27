1087b7ff1141b0e69190ed44ae334eadfe61db61
Kritik hedau <kritikhedau@gmail.com>
2025-01-27 22:26:55 +0530

    Final Commit

--- Advanced/Recursion.js
+++ Advanced/Recursion.js
@@ -1,10 +1,9 @@
-function factorial(num) { 
+//  4. Write a recursive function to calculate the factorial of a given number.
 
-  if (num <= 1) return 1; 
+function factorial(num) {
+  if (num <= 1) return 1;
 
-  return num * factorial(num - 1); 
+  return num * factorial(num - 1);
+}
 
-} 
-
-
-console.log(factorial(5))
+console.log(factorial(5));
