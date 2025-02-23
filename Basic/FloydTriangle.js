581a35bfcd0a6b2857c96d4a13dddb39df95911a
Kritik hedau <kritikhedau@gmail.com>
2025-02-24 01:00:29 +0530

    Floyed Triangle Added

--- Basic/FloydTriangle.js
+++ Basic/FloydTriangle.js
@@ -1,15 +1,32 @@
+// function printTriangle(rows){
+//   let count = 1
+//   for(let i = 1; i <= rows; i++){
+//     let row = ""
+//     for(let j = 1; j <= i; j++){
+//       row += count + " "
+//       count++
+//     }
+//     console.log(row);
+//   }
+// }
 
-function printTriangle(rows){
-  let count = 1
-  for(let i = 1; i <= rows; i++){
-    let row = ""
-    for(let j = 1; j <= i; j++){
-      row += count + " "
-      count++
-    }
-    console.log(row);
+// // console.log(printTriangle(5));
+// printTriangle(5)
+
+
+
+function printTriangle(rows,currentRows=1,number=1){
+  if(currentRows > rows){
+    return;
   }
+  let row = ""
+  for(let i = 1; i <= currentRows; i++){
+    row += number + " "
+    number++
+  }
+  console.log(row);
+  printTriangle(rows,currentRows + 1,number)
 }
 
-// console.log(printTriangle(5));
 printTriangle(5)
+
